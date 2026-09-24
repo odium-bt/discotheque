8d6c3213e442d54b4436f203a35da790022a5474
Zadig Mouquet <zadig.mouquet@gmail.com>
2026-09-24 12:17:50 +0200

    Créé le menu principal

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -4,26 +4,18 @@ import java.util.Scanner;
 
 public class Controller {
     static Scanner scan = new Scanner(System.in);
-    static int choix = 0;
 
     /**
      * Affiche le menu principal
      */
-    public void afficherMenu() {
+    public int afficherMenu() {
         System.out.println("===== GESTION DE LA DISCOTHEQUE =====");
-        System.out.println("1. Albums");
-        System.out.println("2. Disques vinyles");
-        System.out.println("");
-        System.out.println("3. Fichiers numériques");
-        System.out.println("6. Disques compacts");
+        System.out.println("1. Ajouter un album");
+        System.out.println("2. Supprimer un album");
         System.out.println("3. Afficher le contenu de la discotheque");
         System.out.println("4. Vider la discotheque");
         System.out.println("0. Quitter");
-
         System.out.print("Choix:");
-        choix = Controller.scan.nextInt();
-        switch (choix){
-            case 4
-        }
+        return Controller.scan.nextInt();
     }
 }
