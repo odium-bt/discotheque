bf4dc3325e84187454520be247235bfadc6d4a44
Zadig Mouquet <zadig.mouquet@gmail.com>
2026-09-25 14:51:44 +0200

    commented options for unfinished features, added checks for file extension, started lectureAlbum()

--- src/main/java/application/Controller.java
+++ src/main/java/application/Controller.java
@@ -28,10 +28,11 @@ public class Controller {
         System.out.println("3. Afficher le contenu de la discotheque");
         System.out.println("4. Vider la discotheque");
         System.out.println("5. Rechercher un album");
-        System.out.println("6. Modifier la quantité d'un album");
-        System.out.println("7. Lister les albums par support");
-        System.out.println("8. Trier les albums");
-        System.out.println("9. Sauvegarder et recharger la discothèque dans un fichier texte");
+        System.out.println("6. Écouter un album (MP3)");
+        //System.out.println("6. Modifier la quantité d'un album");
+        //System.out.println("7. Lister les albums par support");
+        //System.out.println("8. Trier les albums");
+        //System.out.println("9. Sauvegarder et recharger la discothèque dans un fichier texte");
         System.out.println("0. Quitter");
         System.out.print("Choix:");
         try {
@@ -150,7 +151,15 @@ public class Controller {
     }
 
     public String saisieFormat() throws SaisieInvalideException {
-        return saisieStr("Format du fichier (ex: mp3) : ");
+        String ext = saisieStr("Format du fichier (ex: mp3) : ").toLowerCase();
+        if (switch (ext) {
+            case "mp3", "flac", "wav", "aac" -> true;
+            default -> false;
+        }) {
+            return ext;
+        } else {
+            throw new SaisieInvalideException("Format de fichier invalide");
+        }
     }
 
     public int saisieDuree() throws SaisieInvalideException {
@@ -239,5 +248,10 @@ public class Controller {
         }
     }
 
-
+    /**
+     * Lecture d'album
+     */
+    public void lectureAlbum() throws SaisieInvalideException {
+        String name = saisieNomA();
+    }
 }
